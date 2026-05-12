4748f57d1b1cb65e60b350a0475f3edfdad10d26
vincent.encrypt <VincentNaujoks@proton.me>
2026-05-12 12:38:39 +0200

    Update build.sh to remove icon option because it won't work without a icon
    Removed icon option from pyinstaller command.

--- build.sh
+++ build.sh
@@ -12,8 +12,7 @@ rm -rf build dist software.spec
 pyinstaller software.py \
   --onedir \
   --windowed \
-  --name software \
-  --icon icon.icns
+  --name software
 
 plutil -replace LSUIElement -bool true dist/software.app/Contents/Info.plist
 
