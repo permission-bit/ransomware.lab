661644c70206bb02854e15443a9fb06187fe033d
Vincent <vincent24022002@icloud.com>
2026-05-26 11:38:02 +0200

    add function to get transfer amount depands on upload time

--- server/get_importent_files.py
+++ server/get_importent_files.py
@@ -3,6 +3,7 @@ import os
 import stat
 from datetime import datetime
 import time
+import speedtest
 from server.get_importent_files import get_importent_files_by_extansion, make_file_size_beauty, get_biggest, last_changed, get_importent_developer_files, get_folder_size
 
 IMPORTANT_FILES = get_importent_files_by_extansion
@@ -36,6 +37,42 @@ IMPORTANT_EXTENSIONS = {
     ".yaml",
     ".yml",
 }
+
+MAX_SECONDS = 120
+MIN_SPEED_MBIT = 5
+
+def measure_upload_mbit() -> float:
+    s = speedtest.Speedtest()
+    s.get_best_server()
+    upload_bps = s.upload()
+    return upload_bps / 1_000_000  # bit/s -> Mbit/s
+
+
+def get_transfer_amount(upload_speed_mbit: float) -> int:
+    if upload_speed_mbit < MIN_SPEED_MBIT:
+        return 100 * 1024 * 1024  # 100 MB fallback
+
+    bytes_per_second = (upload_speed_mbit * 1_000_000) / 8
+    return int(bytes_per_second * MAX_SECONDS)
+
+
+def messure_upload():
+
+    try:
+        speed = measure_upload_mbit()
+        #print(f"Upload: {speed:.2f} Mbit/s")
+
+        transfer_amount = get_transfer_amount(speed)
+
+        # with open("transfer_amount.json", "w") as f:
+        #     json.dump({"transfer_bytes": transfer_amount}, f, indent=2)
+
+    except Exception as e:
+        #print("Speedtest failed:", e)
+        return transfer_amount
+
+
+
 
 def get_importent_files_by_extansion():
 
